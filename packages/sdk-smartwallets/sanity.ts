6d088404fbb162b0ea7f5c6c9418e5d220edb403
sam bacha <sam@manifoldfinance.com>
2021-10-06 17:46:35 -0700

    feat(sdk-smartcontracts): init pkg

---
+++ packages/sdk-smartwallets/sanity.ts
@@ -0,0 +1,12 @@
+/**
+* @const isSmartContract
+* @returns boolean
+* @summary checks if dealing with EOA or Smart Contract
+*/
+import { providers, utils } from "ethers";
+
+const provider = new providers.JsonRpcProvider(rpcUrl);
+
+const bytecode = await provider.getCode(address);
+
+const isSmartContract = bytecode && utils.hexStripZeros(bytecode) !== "0x";
