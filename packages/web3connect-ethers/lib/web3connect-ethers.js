d8adc8f5d4be2d28e0e7034ab179b44688ebf649
sam bacha <sam@manifoldfinance.com>
2021-10-11 02:05:05 -0700

    feat(wallets): integrate
    Signed-off-by: sam bacha <sam@manifoldfinance.com>

--- packages/web3connect-ethers/lib/web3connect-ethers.js
+++ packages/web3connect-ethers/lib/web3connect-ethers.js
@@ -1,7 +1,36 @@
-'use strict';
+import { providers } from "ethers";
 
-module.exports = web3connectEthers;
+//  Wrap with Web3Provider from ethers.js
+const web3Provider = new providers.Web3Provider(provider);
 
-function web3connectEthers() {
-    // TODO
-}
+
+import WalletConnectProvider from "@walletconnect/web3-provider";
+
+//  Create WalletConnect Provider
+const provider = new WalletConnectProvider({
+  rpc: {
+    1: "https://api.staging.sushirelay.com/v1"
+  },
+  qrcodeModalOptions: {
+    mobileLinks: ["rainbow", "metamask", "argent", "trust", "imtoken", "pillar", "dharma", "gnosissafe", "ledgerlive", "1inch", "GridPlus", "Torus"],
+},
+});
+
+//  Enable session (triggers QR Code modal)
+await provider.enable();
+
+
+// Subscribe to accounts change
+provider.on("accountsChanged", (accounts: string[]) => {
+  console.log(accounts);
+});
+
+// Subscribe to chainId change
+provider.on("chainChanged", (chainId: number) => {
+  console.log(chainId);
+});
+
+// Subscribe to session disconnection
+provider.on("disconnect", (code: number, reason: string) => {
+  console.log(code, reason);
+});
--- packages/web3connect-web3js/lib/web3connect-web3js.js
+++ packages/web3connect-web3js/lib/web3connect-web3js.js
@@ -1,7 +1,36 @@
-'use strict';
+import Web3 from "web3";
 
-module.exports = web3connectWeb3js;
+//  Create Web3 instance
+const web3 = new Web3(provider);
 
-function web3connectWeb3js() {
-    // TODO
-}
+
+import WalletConnectProvider from "@walletconnect/web3-provider";
+
+//  Create WalletConnect Provider
+const provider = new WalletConnectProvider({
+  rpc: {
+    1: "https://api.staging.sushirelay.com/v1"
+  },
+  qrcodeModalOptions: {
+    mobileLinks: ["rainbow", "metamask", "argent", "trust", "imtoken", "pillar", "dharma", "gnosissafe", "ledgerlive", "1inch", "GridPlus", "Torus"],
+},
+});
+
+//  Enable session (triggers QR Code modal)
+await provider.enable();
+
+
+// Subscribe to accounts change
+provider.on("accountsChanged", (accounts: string[]) => {
+  console.log(accounts);
+});
+
+// Subscribe to chainId change
+provider.on("chainChanged", (chainId: number) => {
+  console.log(chainId);
+});
+
+// Subscribe to session disconnection
+provider.on("disconnect", (code: number, reason: string) => {
+  console.log(code, reason);
+});
