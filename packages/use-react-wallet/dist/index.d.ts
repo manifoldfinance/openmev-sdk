aed88c215fee4d788f44c507c423017fa6f8fe3d
sam bacha <sam@manifoldfinance.com>
2021-11-07 03:14:43 -0800

    fix(use-react-wallet): resolve package dist error

--- packages/use-react-wallet/dist/index.d.ts
+++ packages/use-react-wallet/dist/index.d.ts
@@ -1,34 +1,17 @@
-/// <reference types="node" />
-import type { State, StoreApi } from 'zustand/vanilla';
-import type { EventEmitter } from 'node:events';
-export interface Web3ReactState extends State {
-    chainId: number | undefined;
-    accounts: string[] | undefined;
-    activating: boolean;
-    error: Error | undefined;
-}
-export declare type Web3ReactStore = StoreApi<Web3ReactState>;
-export interface Web3ReactStateUpdate {
-    chainId?: number;
-    accounts?: string[];
-}
-export interface Actions {
-    startActivation: () => void;
-    update: (stateUpdate: Web3ReactStateUpdate) => void;
-    reportError: (error: Error) => void;
-    reset: () => void;
-}
-export interface RequestArguments {
-    readonly method: string;
-    readonly params?: readonly unknown[] | object;
-}
-export interface Provider extends EventEmitter {
-    request(args: RequestArguments): Promise<unknown>;
-}
-export declare abstract class Connector {
-    provider: Provider | undefined;
-    protected readonly actions: Actions;
-    constructor(actions: Actions);
-    abstract activate(...args: any[]): Promise<void> | void;
-    deactivate?(...args: any[]): Promise<void> | void;
-}
+import Web3Modal, { ICoreOptions } from 'web3modal';
+import { Network, Web3Provider } from '@ethersproject/providers';
+declare type State = {
+    provider?: Web3Provider;
+    account?: Account;
+    network?: Network;
+    web3Modal?: Web3Modal;
+};
+declare type Account = string;
+declare type ConnectWallet = (opts?: Partial<ICoreOptions>) => void;
+declare type DisconnectWallet = () => void;
+declare type UseWallet = () => State & {
+    connect: ConnectWallet;
+    disconnect: DisconnectWallet;
+};
+export declare const useWallet: UseWallet;
+export {};
--- packages/use-react-wallet/src/index.ts
+++ packages/use-react-wallet/src/index.ts
@@ -1,47 +1,108 @@
-import type { State, StoreApi } from 'zustand/vanilla'
-import type { EventEmitter } from 'node:events'
+/**
+ *  @package useWallet
+ *  @since 0.1.0
+ *  @link
+ *  @version 0.1.0
+ *
+ */
 
-export interface Web3ReactState extends State {
-  chainId: number | undefined
-  accounts: string[] | undefined
-  activating: boolean
-  error: Error | undefined
-}
+import Web3Modal, { ICoreOptions } from 'web3modal';
+import { Network, Web3Provider } from '@ethersproject/providers';
+import create from 'zustand/esm';
+import { useEffect } from 'react';
 
-export type Web3ReactStore = StoreApi<Web3ReactState>
+type State = {
+  provider?: Web3Provider;
+  account?: Account;
+  network?: Network;
+  web3Modal?: Web3Modal;
+};
 
-export interface Web3ReactStateUpdate {
-  chainId?: number
-  accounts?: string[]
-}
+const useStore = create<State>((_set) => ({}));
 
-export interface Actions {
-  startActivation: () => void
-  update: (stateUpdate: Web3ReactStateUpdate) => void
-  reportError: (error: Error) => void
-  reset: () => void
-}
+type Account = string;
+type ConnectWallet = (opts?: Partial<ICoreOptions>) => void;
+type DisconnectWallet = () => void;
+type UseWallet = () => State & {
+  connect: ConnectWallet;
+  disconnect: DisconnectWallet;
+};
 
-// per EIP-1193
-export interface RequestArguments {
-  readonly method: string
-  readonly params?: readonly unknown[] | object
-}
+export const useWallet: UseWallet = () => {
+  /**
+   * @useWallet
+   * @summary Retrieve the current values from the store, & automatically re-render on updates
+   * @const account
+   * @const network
+   * @const provider
+   * @const web3Modal
+   */
+  const account = useStore((state) => state.account);
+  const network = useStore((state) => state.network);
+  const provider = useStore((state) => state.provider);
+  const web3Modal = useStore((state) => state.web3Modal);
 
-// per EIP-1193
-export interface Provider extends EventEmitter {
-  request(args: RequestArguments): Promise<unknown>
-}
+  useEffect(() => {
+    useStore.setState({ web3Modal: new Web3Modal() });
+  }, []);
 
-export abstract class Connector {
-  public provider: Provider | undefined
+  const connect: ConnectWallet = async (opts) => {
+    /**
+     * setState
+     * @description Launch modal with the given options
+     */
+    const web3Modal = new Web3Modal(opts);
+    useStore.setState({ web3Modal });
+    const web3ModalProvider = await web3Modal.connect();
 
-  protected readonly actions: Actions
+    /**
+     * @const initialProvider
+     * @description Set up Ethers provider and initial state with the response from the web3Modal
+     */
+    const initialProvider = new Web3Provider(web3ModalProvider, 'any');
+    const getNetwork = () => initialProvider.getNetwork();
+    const initialAccounts = await initialProvider.listAccounts();
+    const initialNetwork = await getNetwork();
+    useStore.setState({
+      provider: initialProvider,
+      network: initialNetwork,
+      account: initialAccounts[0],
+    });
 
-  constructor(actions: Actions) {
-    this.actions = actions
-  }
+    /**
+     *
+     *  Set up event listeners to handle state changes
+     *
+     */
+    web3ModalProvider.on('accountsChanged', (accounts: string[]) => {
+      useStore.setState({ account: accounts[0] });
+    });
 
-  public abstract activate(...args: any[]): Promise<void> | void
-  public deactivate?(...args: any[]): Promise<void> | void
-}
+    web3ModalProvider.on('chainChanged', async (_chainId: string) => {
+      const network = await getNetwork();
+      useStore.setState({ network });
+    });
+
+    web3ModalProvider.on('disconnect', () => {
+      web3Modal.clearCachedProvider();
+    });
+  };
+
+  const disconnect: DisconnectWallet = async () => {
+    web3Modal?.clearCachedProvider();
+    useStore.setState({
+      provider: undefined,
+      network: undefined,
+      account: undefined,
+    });
+  };
+
+  return {
+    connect,
+    provider,
+    account,
+    network,
+    disconnect,
+    web3Modal,
+  };
+};
