622ab1060673699f0100399db6dc2ce105dd67d1
sam bacha <sam@manifoldfinance.com>
2021-11-07 01:07:13 -0800

    chore(dist): compiled output

--- packages/use-react-wallet/dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./use-react-wallet.cjs.production.min.js')
-} else {
-  module.exports = require('./use-react-wallet.cjs.development.js')
-}
--- packages/use-react-wallet/dist/use-react-wallet.esm.js
+++
@@ -1,6 +0,0 @@
-var Connector = function Connector(actions) {
-  this.actions = actions;
-};
-
-export { Connector };
-//# sourceMappingURL=use-react-wallet.esm.js.map
